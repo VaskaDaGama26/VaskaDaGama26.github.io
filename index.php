a37f66ca8f0e09c60c449abd118821108eb64d7d
VaskaDaGama26 <131616200+VaskaDaGama26@users.noreply.github.com>
2023-04-23 22:20:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <link rel="stylesheet" href="style.css">
+    <title>Главная</title>
+</head>
+<body>
+<header>
+<nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="menu">
+                    <ul>
+                        <li><a href="index.php"><img class="nav-logo" src="images/logo.png"></a></li>
+                        <li><a class="nav-active" href="">Главная</a></li>
+                        <li><a class="nav" href="services.php">Услуги</a></li>
+                        <li><a class="nav" href="about.php">О компании</a></li>
+                        <li><a class="nav" href="contacts.php">Контакты</a></li>
+                        <li><a class="nav-form"href="user/sign-in.php">Войти</a></li>
+                        <li><a class="nav-form" href="user/sign-up.php">Зарегистрироваться</a></li>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
+ <!-- <div class="p-5 text-center bg-light" style="margin-top: 58px;">
+    <h1 class="mb-3">Heading</h1>
+    <h4 class="mb-3">Subheading</h4>
+    <a class="btn btn-primary" href="" role="button">Call to action</a>
+  </div> -->
+</header>
+<section>
+<div class="testdiv">
+    <div class="test"><p>ГЛАВНАЯ</p></div>
+</div>
+</section>
+<footer>
+
+</footer>
+</body>
+</html>
